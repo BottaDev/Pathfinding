54086f3ab450e1bfa99e5e86590d9914bb4adef2
BottaDev <fedebotta@gmail.com>
2021-05-28 20:15:15 -0300

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Speed * Time.deltaTime * Vector3.back;
+        else if (Input.GetKey(KeyCode.S))
+            transform.position += Speed * Time.deltaTime * Vector3.forward;
+        
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Speed * Time.deltaTime * Vector3.right;
+        else if (Input.GetKey(KeyCode.D))
+            transform.position += Speed * Time.deltaTime * Vector3.left;
+    }
+}
